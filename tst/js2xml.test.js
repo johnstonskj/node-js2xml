ffdf6b55abf429099b02e14707117e5a9ecc6a04
Simon Johnston <simon@johnstonshome.org>
2012-11-06 11:47:38 -0800

    Removed dependency on genx, implemented own pure-JS XmlWriter.

--- tst/js2xml.test.js
+++ tst/js2xml.test.js
@@ -9,7 +9,7 @@ describe('js2xml', function() {
             var output = xml.toXml(null);
             Assert.strictEqual(
                 output,
-                '<js:Object xmlns:js="http://www.example.org/js2xml" id="1"></js:Object>',
+                '<?xml version="1.0" encoding="UTF-8" ?><js:Object xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:js="http://www.example.org/js2xml" id="1"/>',
                 'Badly generated XML');
             done();
         });
@@ -18,7 +18,7 @@ describe('js2xml', function() {
             var output = xml.toXml(undefined);
             Assert.strictEqual(
                 output,
-                '<js:Object xmlns:js="http://www.example.org/js2xml" id="1"></js:Object>',
+                '<?xml version="1.0" encoding="UTF-8" ?><js:Object xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:js="http://www.example.org/js2xml" id="1"/>',
                 'Badly generated XML');
             done();
         });
@@ -27,7 +27,7 @@ describe('js2xml', function() {
             var output = xml.toXml('root');
             Assert.strictEqual(
                 output,
-                '<js:value xmlns:js="http://www.example.org/js2xml">root</js:value>',
+                '<?xml version="1.0" encoding="UTF-8" ?><js:value xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:js="http://www.example.org/js2xml">root</js:value>',
                 'Badly generated XML');
             done();
         });
@@ -36,7 +36,7 @@ describe('js2xml', function() {
             var output = xml.toXml(42);
             Assert.strictEqual(
                 output,
-                '<js:value xmlns:js="http://www.example.org/js2xml" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="xsd:integer">42</js:value>',
+                '<?xml version="1.0" encoding="UTF-8" ?><js:value xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:js="http://www.example.org/js2xml" xsi:type="xsd:integer">42</js:value>',
                 'Badly generated XML');
             done();
         });
@@ -45,7 +45,7 @@ describe('js2xml', function() {
             var output = xml.toXml(new Date(2012, 10, 01, 08, 54, 49));
             Assert.strictEqual(
                 output,
-                '<js:value xmlns:js="http://www.example.org/js2xml" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="xsd:dateTime">2012-11-01T08:54:49-0700</js:value>',
+                '<?xml version="1.0" encoding="UTF-8" ?><js:value xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:js="http://www.example.org/js2xml" xsi:type="xsd:dateTime">2012-11-01T08:54:49-0700</js:value>',
                 'Badly generated XML');
             done();
         });
@@ -54,7 +54,7 @@ describe('js2xml', function() {
             var output = xml.toXml([1, 2, 3, 4]);
             Assert.strictEqual(
                 output,
-                '<js:Array xmlns:js="http://www.example.org/js2xml"><_0 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="xsd:integer">1</_0><_1 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="xsd:integer">2</_1><_2 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="xsd:integer">3</_2><_3 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="xsd:integer">4</_3></js:Array>',
+                '<?xml version="1.0" encoding="UTF-8" ?><js:Array xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:js="http://www.example.org/js2xml"><_0 xsi:type="xsd:integer">1</_0><_1 xsi:type="xsd:integer">2</_1><_2 xsi:type="xsd:integer">3</_2><_3 xsi:type="xsd:integer">4</_3></js:Array>',
                 'Badly generated XML');
             done();
         });
@@ -67,7 +67,7 @@ describe('js2xml', function() {
             });
             Assert.strictEqual(
                 output,
-                '<js:Object xmlns:js="http://www.example.org/js2xml" id="1"><str>string</str><num xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="xsd:integer">42</num><bool xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="xsd:boolean">true</bool></js:Object>',
+                '<?xml version="1.0" encoding="UTF-8" ?><js:Object xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:js="http://www.example.org/js2xml" id="1"><str>string</str><num xsi:type="xsd:integer">42</num><bool xsi:type="xsd:boolean">true</bool></js:Object>',
                 'Badly generated XML');
             done();
         });
@@ -83,7 +83,7 @@ describe('js2xml', function() {
             });
             Assert.strictEqual(
                 output,
-                '<js:Object xmlns:js="http://www.example.org/js2xml" id="1"><object idref="1"></object><array><_0 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="xsd:integer">1</_0><_1 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="xsd:integer">2</_1><_2 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="xsd:integer">3</_2><_3 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="xsd:integer">4</_3></array></js:Object>',
+                '<?xml version="1.0" encoding="UTF-8" ?><js:Object xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:js="http://www.example.org/js2xml" id="1"><object idref="1"/><array><_0 xsi:type="xsd:integer">1</_0><_1 xsi:type="xsd:integer">2</_1><_2 xsi:type="xsd:integer">3</_2><_3 xsi:type="xsd:integer">4</_3></array></js:Object>',
                 'Badly generated XML');
             done();
         });
@@ -98,7 +98,7 @@ describe('js2xml', function() {
             var output = xml.toXml(object);
             Assert.strictEqual(
                 output,
-                '<js:Object xmlns:js="http://www.example.org/js2xml" id="1"><str>string</str><num xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="xsd:integer">42</num><bool xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="xsd:boolean">true</bool><self idref="1"></self></js:Object>',
+                '<?xml version="1.0" encoding="UTF-8" ?><js:Object xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:js="http://www.example.org/js2xml" id="1"><str>string</str><num xsi:type="xsd:integer">42</num><bool xsi:type="xsd:boolean">true</bool><self idref="1"/></js:Object>',
                 'Badly generated XML');
             done();
         });
@@ -107,7 +107,7 @@ describe('js2xml', function() {
             var output = xml.toXml({name: 'value'}, {rootName: 'root'});
             Assert.strictEqual(
                 output,
-                '<root id="1"><name>value</name></root>',
+                '<?xml version="1.0" encoding="UTF-8" ?><root xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:js="http://www.example.org/js2xml" id="1"><name>value</name></root>',
                 'Badly generated XML');
             done();
         });
@@ -116,7 +116,7 @@ describe('js2xml', function() {
             var output = xml.toXml({name: 'value'}, {defaultUrl: 'http://example.com/myNamespace', defaultPrefix: 'mine'});
             Assert.strictEqual(
                 output,
-                '<mine:Object xmlns:mine="http://example.com/myNamespace" id="1"><name>value</name></mine:Object>',
+                '<?xml version="1.0" encoding="UTF-8" ?><mine:Object xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:mine="http://example.com/myNamespace" id="1"><name>value</name></mine:Object>',
                 'Badly generated XML');
             done();
         });
---
+++ tst/writer.test.js
@@ -0,0 +1,179 @@
+var Assert = require('assert');
+var XmlWriter = require('../lib/writer').XmlWriter;
+
+describe('XmlWriter', function() {
+
+    describe('#()', function() {
+
+        it('No options', function(done) {
+            var writer = new XmlWriter();
+            writer.startDocument();
+            var element = writer.startElement('Test');
+            element.endElement();
+            writer.endDocument();
+            Assert.strictEqual(
+                writer.toString(),
+                '<?xml version="1.0" encoding="UTF-8" ?><Test/>',
+                'Badly generated XML');
+            done();
+        });
+
+        it('Set XML pi version', function(done) {
+            var writer = new XmlWriter({version: '2.0'});
+            writer.startDocument();
+            var element = writer.startElement('Test');
+            element.endElement();
+            writer.endDocument();
+            Assert.strictEqual(
+                writer.toString(),
+                '<?xml version="2.0" encoding="UTF-8" ?><Test/>',
+                'Badly generated XML');
+            done();
+        });
+
+        it('Set XML pi encoding', function(done) {
+            var writer = new XmlWriter({encoding: 'UTF-16'});
+            writer.startDocument();
+            var element = writer.startElement('Test');
+            element.endElement();
+            writer.endDocument();
+            Assert.strictEqual(
+                writer.toString(),
+                '<?xml version="1.0" encoding="UTF-16" ?><Test/>',
+                'Badly generated XML');
+            done();
+        });
+
+    });
+
+    describe('#endDocument()', function() {
+
+        it('No root element, or startDocument', function(done) {
+            var writer = new XmlWriter();
+            Assert.throws(function() {
+                    writer.endDocument();
+                },
+                Error,
+                'Should have thrown an Error');
+            done();
+        });
+
+        it('No root element', function(done) {
+            var writer = new XmlWriter();
+            writer.startDocument();
+            Assert.throws(function() {
+                    writer.endDocument();
+                },
+                Error,
+                'Should have thrown an Error');
+            done();
+        });
+
+        it('Root element still open', function(done) {
+            var writer = new XmlWriter();
+            writer.startDocument();
+            var element = writer.startElement('Test');
+            Assert.throws(function() {
+                    writer.endDocument();
+                },
+                Error,
+                'Should have thrown an Error');
+            done();
+        });
+
+    });
+
+    describe('#addNamespace()', function() {
+
+        it('adding root namespaces, before', function(done) {
+            var writer = new XmlWriter({encoding: 'UTF-16'});
+            writer.startDocument();
+            writer.addNamespace('http://example.org/people', 'people');
+            writer.addNamespace('http://example.org/places', 'places');
+            var element = writer.startElement('Test');
+            element.endElement();
+            writer.endDocument();
+            Assert.strictEqual(
+                writer.toString(),
+                '<?xml version="1.0" encoding="UTF-16" ?><Test xmlns:people="http://example.org/people" xmlns:places="http://example.org/places"/>',
+                'Badly generated XML');
+            done();
+        });
+
+        it('adding root namespaces, after', function(done) {
+            var writer = new XmlWriter({encoding: 'UTF-16'});
+            writer.startDocument();
+            var element = writer.startElement('Test');
+            writer.addNamespace('http://example.org/people', 'people');
+            writer.addNamespace('http://example.org/places', 'places');
+            element.endElement();
+            writer.endDocument();
+            Assert.strictEqual(
+                writer.toString(),
+                '<?xml version="1.0" encoding="UTF-16" ?><Test xmlns:people="http://example.org/people" xmlns:places="http://example.org/places"/>',
+                'Badly generated XML');
+            done();
+        });
+
+        it('adding root, and element, namespaces', function(done) {
+            var writer = new XmlWriter({encoding: 'UTF-16'});
+            writer.startDocument();
+            writer.addNamespace('http://example.org/people', 'people');
+            writer.addNamespace('http://example.org/places', 'places');
+            var element = writer.startElement('Test');
+            element.addNamespace('http://example.org/things', 'things');
+            element.endElement();
+            writer.endDocument();
+            Assert.strictEqual(
+                writer.toString(),
+                '<?xml version="1.0" encoding="UTF-16" ?><Test xmlns:people="http://example.org/people" xmlns:places="http://example.org/places" xmlns:things="http://example.org/things"/>',
+                'Badly generated XML');
+            done();
+        });
+
+    });
+
+    describe('#addAttribute()', function() {
+
+        it('Add simple attribute', function(done) {
+            var writer = new XmlWriter({encoding: 'UTF-16'});
+            writer.startDocument();
+            var element = writer.startElement('Test');
+            element.addAttribute('id', '987245');
+            element.endElement();
+            writer.endDocument();
+            Assert.strictEqual(
+                writer.toString(),
+                '<?xml version="1.0" encoding="UTF-16" ?><Test id="987245"/>',
+                'Badly generated XML');
+            done();
+        });
+
+    });
+
+    describe('#startElement()', function() {
+
+        it('Add nested elements', function(done) {
+            var writer = new XmlWriter({encoding: 'UTF-16'});
+            writer.startDocument();
+            for (var i = 0; i < 5; i++) {
+                var element = writer.startElement('Test');
+                element.addAttribute('id', i.toString());
+                for (var x = 0; x < 2; x++) {
+                    var child = element.startElement('child');
+                    child.addAttribute('id', i.toString() + '-' + x.toString());
+                    child.endElement();
+                }
+                element.endElement();
+            }
+            writer.endDocument();
+            Assert.strictEqual(
+                writer.toString(),
+                '<?xml version="1.0" encoding="UTF-16" ?><Test id="0"><child id="0-0"/><child id="0-1"/></Test><Test id="1"><child id="1-0"/><child id="1-1"/></Test><Test id="2"><child id="2-0"/><child id="2-1"/></Test><Test id="3"><child id="3-0"/><child id="3-1"/></Test><Test id="4"><child id="4-0"/><child id="4-1"/></Test>',
+                'Badly generated XML');
+            done();
+        });
+
+    });
+
+});
